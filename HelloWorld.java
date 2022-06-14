e6f50323da52ecafd9d107cee912dc88546515fb
koushikgajulapalle22 <107239269+koushikgajulapalle22@users.noreply.github.com>
2022-06-14 17:38:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("My name is Deeksha");
+    System.out.println("My name is Koushik Gajulapalle");
   }
 }
